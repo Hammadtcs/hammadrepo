48d6bdc07304e9a23a149713dd2816df0758b5a5
hammad.bin.mohd@outlook.com <hammad.bin.mohd@outlook.com>
2025-05-08 21:35:51 +0530

    added html

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Enable serving files from wwwroot
+app.UseStaticFiles();
+
+// Fallback so that any unknown route serves index.html
+app.MapFallbackToFile("index.html");
 
 app.Run();
