cbbe8ee12ab177735acec915893f065f23bba43e
hammad.bin.mohd@outlook.com <hammad.bin.mohd@outlook.com>
2025-05-17 04:20:12 +0530

    Commited Rotawebapp sapp

---
+++ RotasWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RotasWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
