3d6163e8efb5cc54744f419d425b117f17ed439f
hammad.bin.mohd@outlook.com <hammad.bin.mohd@outlook.com>
2025-05-18 01:47:16 +0530

    SatorCommit

---
+++ SatorAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SatorAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
