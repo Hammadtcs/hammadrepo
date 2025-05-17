98e3fe2795206089153adeb9f0d8c8429030c6ab
Hammad Bin Mohd <hammad.bin.mohd@outlook.com>
2025-05-17 21:21:53 +0000

    Deleted WebApplication2

--- WebApplication2/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles(); // Enables index.html by default
-app.UseStaticFiles();  // Serves files from wwwroot
-
-app.Run();
