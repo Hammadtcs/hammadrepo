8d146ed5ca3edfa284b8cc8ebd92e03282191db3
hammad.bin.mohd@outlook.com <hammad.bin.mohd@outlook.com>
2025-05-24 02:54:03 +0530

    Commit for ProjectX

---
+++ ProjectX_Hammad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectX_Hammad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
